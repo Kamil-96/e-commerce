6ee4b0103c7a489922383404a63fd79596aba585
Kamil Put <kamil.put@interia.pl>
2021-10-21 08:09:25 +0200

    Finish initializing project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { MainLayout } from './components/layout/MainLayout/MainLayout';
+import { Home } from './components/views/Home/Home';
+import { store } from './redux/store';
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <MainLayout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
+  </Provider>  //eslint-disable-line
+);
 
 export default App;
